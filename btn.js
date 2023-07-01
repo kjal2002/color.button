cea0de266a93e923207a4722898993ab8ba13f95
kjal2002 <99879312+kjal2002@users.noreply.github.com>
2023-07-01 14:56:47 +0530

    Add files via upload

---
+++ btn.js
@@ -0,0 +1,28 @@
+content=document.getElementById("content")
+
+let RED=document.getElementById("red");
+RED.onclick=function(){
+	box.style.backgroundColor="red";
+	}
+let BLUE=document.getElementById("blue");
+BLUE.onclick=function(){
+	box.style.backgroundColor="blue";
+	}
+let GREEN=document.getElementById("green");
+GREEN.onclick=function(){
+	box.style.backgroundColor="green";
+	}
+let YELLOW=document.getElementById("yellow");
+YELLOW.onclick=function(){
+	box.style.backgroundColor="yellow";
+	}
+	
+//let change_text	= document.getElementById("change");
+//let button1=document.getElementById("btton");
+
+let change_content=document.getElementById("click")
+change_content.onclick=function(){
+	content.innerHTML="hello i am kajal singh";
+};
+
+	
